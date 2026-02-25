60b6c693629c83e9d34c1ed564ad65c841584442
Kaπ <89849620+K4-pi@users.noreply.github.com>
2026-02-25 23:05:05 +0100

    basic gui + server side
    basic gui for start and prototype on server side

---
+++ Chat/Chat/LoginWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+
+namespace Chat
+{
+    public sealed partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();             
+             
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+
+            Microsoft.UI.Windowing.AppWindow appWindow =
+                Microsoft.UI.Windowing.AppWindow.GetFromWindowId(
+                    Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd)
+                );
+
+            appWindow.Resize(new Windows.Graphics.SizeInt32(450, 600));
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var chatWindow = new MainWindow();
+            chatWindow.Activate();
+            this.Close();
+        }
+    }
+}
---
+++ ChatServer/ChatServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var listener = new TcpListener(IPAddress.Any, 9000);
+listener.Start();
+Console.WriteLine("Server started");
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    _ = ClientHandler(client);
+}
+
+async Task ClientHandler(TcpClient client)
+{
+    var stream = client.GetStream();
+    var buffer = new byte[4096];
+
+    try
+    {
+        while (true)
+        {
+            int bytesRead = await stream.ReadAsync(buffer);
+            if (bytesRead == 0)
+                break;
+
+            string msg = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine(msg);
+
+            await stream.WriteAsync(buffer.AsMemory(0, bytesRead));
+        }
+    }
+    finally
+    {
+        client.Close();
+    }
+}
