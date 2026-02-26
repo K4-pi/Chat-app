1c300afd024d36ad6c104d6ab61eacae81e2f004
Kaπ <89849620+K4-pi@users.noreply.github.com>
2026-02-26 19:52:39 +0100

    Authentication server side + Database connection

---
+++ ChatServer/ChatServer/Database.cs
@@ -0,0 +1,60 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ChatServer
+{
+    internal class Database
+    {
+        private IMongoClient client;
+        private IMongoDatabase database;
+
+        public Database()
+        {
+            string connectionUri = "mongodb+srv://chat_db_user:VbAcD6aY3FPrywXd@chatapp.mpve4di.mongodb.net";
+
+            try
+            {
+                client = new MongoClient(connectionUri);
+                database = client.GetDatabase("ChatApp");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Failed to initialize MongoDB: {ex.Message}");
+            }
+        }
+
+        private IMongoCollection<BsonDocument> GetUsersCollection(IMongoDatabase db)
+        {
+            return db.GetCollection<BsonDocument>("users");
+        }
+
+        public String Authenticate(string data)
+        {
+            var credentials = data.Split('@');
+            string username = credentials[0];
+            string password = credentials[1];
+
+            Console.WriteLine($"Username: {username}");
+            Console.WriteLine($"Password: {password}");
+
+            var usersCollection = GetUsersCollection(database);
+
+            var filter = Builders<BsonDocument>.Filter.Eq("Username", username);
+            var userDoc = usersCollection.Find(filter).FirstOrDefault();
+
+            if (userDoc != null && userDoc["Password"] == password)
+            {
+                string sessionId = Guid.NewGuid().ToString();
+                //ActiveSessions.Add(sessionId, username);
+                return $"AUTH_SUCCESS:{sessionId}";
+            }
+
+            return "AUTH_FAIL";
+        }
+
+    }
+}
--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Net;
+﻿using ChatServer;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -17,6 +20,8 @@ async Task ClientHandler(TcpClient client)
     var stream = client.GetStream();
     var buffer = new byte[4096];
 
+    Database database = new Database();
+
     try
     {
         while (true)
@@ -25,10 +30,29 @@ async Task ClientHandler(TcpClient client)
             if (bytesRead == 0)
                 break;
 
-            string msg = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            Console.WriteLine(msg);
+            string msg = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+            Console.WriteLine($"\n{msg}");
 
-            await stream.WriteAsync(buffer.AsMemory(0, bytesRead));
+            string[] result = msg.Split(':');
+            string response = "NO_SUCH_OPERATION";
+
+            switch (result[0])
+            {
+                case "MSG":
+                    Console.WriteLine(result[1]); break;
+                case "LOGIN":
+                    Console.WriteLine("Authentication...");
+                    response = database.Authenticate(result[1]); 
+                    break;
+                default:
+                    Console.WriteLine("No such operation " + result[0]);
+                    Console.WriteLine(msg); 
+                    break;                
+            }
+
+            byte[] responseBuffer = Encoding.UTF8.GetBytes(response);
+            await stream.WriteAsync(responseBuffer, 0, responseBuffer.Length);
+            Console.WriteLine($"Sent: {response}");
         }
     }
     finally
